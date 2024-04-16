626a22fb6f0568505d93f4e50a029b0bcf6b8be1
nftmoondogs <nftmoondogs@gmail.com>
2024-04-16 20:45:40 +0530

    first commit

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -104,7 +104,7 @@ export default function Index() {
     </CardContent>
   </Card>
   <button className="px-8 py-2 rounded-md bg-teal-500 text-white font-bold transition duration-200 hover:bg-white hover:text-black border-2 border-transparent hover:border-teal-500">
-  <Link to="https://pump.fun/82DMnBfwKoBtxVTaTZKw6Ncn9XsYmTq6eAcp32wUfjgG">
+  <Link to="https://pump.fun/board">
         Buy from Pump.fun
         </Link>
       </button>
